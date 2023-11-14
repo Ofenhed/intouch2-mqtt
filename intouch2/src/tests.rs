b3073e7bbbfc6d2a679fc407dd4a944960936bfd
Marcus Ofenhed <marcus@conditionraise.se>
2023-11-14 13:55:47 +0100

    Fixed tests

--- intouch2/src/tests.rs
+++ intouch2/src/tests.rs
@@ -4,67 +4,58 @@ const EMPTY: &[u8] = b"";
 
 #[test]
 fn parse_hello() {
-  assert_eq!(
-    parse_network_data(b"<HELLO>1</HELLO>"),
-    Ok((EMPTY, NetworkPackage::Hello(b"1".to_vec())))
-  );
+  assert!(matches!(
+      parse_network_data(b"<HELLO>1</HELLO>"),
+      Ok(package) if package == NetworkPackage::Hello(b"1".as_slice().into())
+  ));
 }
 
 #[test]
 fn parse_ping_and_pong() {
-  assert_eq!(
-    parse_network_data(b"<PACKT><SRCCN>sender-id</SRCCN><DATAS>APING</DATAS></PACKT>"),
-    Ok((
-      EMPTY,
-      NetworkPackage::Authorized {
-        src: Some(b"sender-id".to_vec()),
-        dst: None,
-        data: NetworkPackageData::Ping
-      }
-    ))
-  );
-  assert_eq!(
-    parse_network_data(
-      b"<PACKT><SRCCN>sender-id</SRCCN><DESCN>receiver-id</DESCN><DATAS>APING\0</DATAS></PACKT>"
-    ),
-    Ok((
-      EMPTY,
-      NetworkPackage::Authorized {
-        src: Some(b"sender-id".to_vec()),
-        dst: Some(b"receiver-id".to_vec()),
-        data: NetworkPackageData::Pong
-      }
-    ))
-  );
+  assert!(matches!(
+      parse_network_data(b"<PACKT><SRCCN>sender-id</SRCCN><DATAS>APING</DATAS></PACKT>"),
+  Ok(package) if package == NetworkPackage::Authorized {
+      src: Some(b"sender-id".as_slice().into()),
+      dst: None,
+      data: NetworkPackageData::Ping
+    }
+  ));
+  assert!(matches!(
+      parse_network_data(
+        b"<PACKT><SRCCN>sender-id</SRCCN><DESCN>receiver-id</DESCN><DATAS>APING\0</DATAS></PACKT>"
+      ),
+  Ok(package) if package == NetworkPackage::Authorized {
+      src: Some(b"sender-id".as_slice().into()),
+      dst: Some(b"receiver-id".as_slice().into()),
+      data: NetworkPackageData::Pong
+    }
+  ));
 }
 
 #[test]
 fn parse_invalid_datas() {
-  assert_eq!(
-    parse_network_data(b"<PACKT><DATAS>APUNG</DATAS></PACKT>"),
-    Ok((
-      EMPTY,
-      NetworkPackage::Authorized {
-        src: None,
-        dst: None,
-        data: NetworkPackageData::Unknown(b"APUNG".to_vec())
-      }
-    ))
-  )
+  assert!(matches!(
+      parse_network_data(b"<PACKT><DATAS>APUNG</DATAS></PACKT>"),
+      Ok(package) if package == NetworkPackage::Authorized {
+      src: None,
+      dst: None,
+      data: NetworkPackageData::Unknown(b"APUNG".as_slice().into())
+    }
+  ))
 }
 
 #[test]
 fn id_packets() {
   let packets = vec![
-    NetworkPackage::Hello(b"My hello".to_vec()),
+    NetworkPackage::Hello(b"My hello".as_slice().into()),
     NetworkPackage::Authorized {
-      src: Some(b"some-src".to_vec()),
+      src: Some(b"some-src".as_slice().into()),
       dst: None,
       data: NetworkPackageData::Ping,
     },
     NetworkPackage::Authorized {
-      src: Some(b"some-src".to_vec()),
-      dst: Some(b"some-dest".to_vec()),
+      src: Some(b"some-src".as_slice().into()),
+      dst: Some(b"some-dest".as_slice().into()),
       data: NetworkPackageData::Pong,
     },
     NetworkPackage::Authorized {
@@ -75,10 +66,10 @@ fn id_packets() {
     NetworkPackage::Authorized {
       src: None,
       dst: None,
-      data: NetworkPackageData::Version(b"some_version".to_vec()),
+      data: NetworkPackageData::Version(b"some_version".as_slice().into()),
     },
     // NetworkPackage::Authorized{src: None, dst: None, data: NetworkPackageData::PushStatus(b"Some
-    // status".to_vec())},
+    // status".as_slice().into())},
     NetworkPackage::Authorized {
       src: None,
       dst: None,
@@ -97,6 +88,6 @@ fn id_packets() {
   ];
   for pkg in packets.into_iter() {
     let composed = compose_network_data(&pkg);
-    assert_eq!(parse_network_data(composed.as_slice()), Ok((EMPTY, pkg)));
+    assert!(matches!(parse_network_data(&composed), Ok(package) if package == pkg));
   }
 }
