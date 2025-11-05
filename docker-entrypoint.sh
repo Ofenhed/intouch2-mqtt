be7b6b98ff31192641b7baba3f1773c8e8941d15
Marcus Ofenhed <marcus.ofenhed@conditionraise.se>
2025-11-05 10:51:40 +0100

    Remove trace prints

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/with-contenv bashio
 
 export MQTT_SERVER="$(bashio::services 'mqtt' 'host'):$(bashio::services 'mqtt' 'port')"
-echo "Got MQTT server $MQTT_SERVER"
 export MQTT_USER="$(bashio::services 'mqtt' 'username')"
-echo "Got MQTT username $MQTT_USER"
 export MQTT_PASSWORD="$(bashio::services 'mqtt' 'password')"
-echo "Got MQTT password"
 
 exec /usr/local/bin/intouch2-mqtt
